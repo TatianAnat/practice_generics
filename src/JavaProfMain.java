e80db73693d79dd3c10874911dd3e4a3ca88c7a0
TatianAnat <TatianAnat@yandex.ru>
2025-07-04 23:06:01 +0400

    practice generics_arr1

--- src/JavaProfMain.java
+++ src/JavaProfMain.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+
 public class JavaProfMain {
     public static void main(String[] args) {
         System.out.println("Skypro Uni - Java Profession start!");
+        //создаём массив из 3-х элементов
+        int[] arr = {1,2,3};
+        //увеличиваем созданный массив в два раза
+        arr = Arrays.copyOf(arr, arr.length*2);
+        //чтоб смогли добавить данный элемент
+        arr[3] = 4;
+
+        System.out.println("Arrays.toString(arr) = " + Arrays.toString(arr));
     }
 }
